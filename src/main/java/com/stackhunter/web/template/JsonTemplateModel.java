c298e625143211611fbeb8e70d8a581254a7481c
Dele Taylor <dele.taylor@northconcepts.com>
2018-09-27 18:52:35 -0400

    Updated libraries

--- src/main/java/com/stackhunter/web/template/JsonTemplateModel.java
+++ src/main/java/com/stackhunter/web/template/JsonTemplateModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 North Concepts Inc.
+ * Copyright (c) 2014-2018 North Concepts Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,7 @@ package com.stackhunter.web.template;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-import org.codehaus.jackson.JsonNode;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import com.stackhunter.web.content.ContentErrorCode;
 import com.stackhunter.web.content.ContentException;
 
