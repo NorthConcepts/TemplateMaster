4cccae7eb6b7b6ad99bcb9aa44c956946693edda
Dele Taylor <dele.taylor@northconcepts.com>
2019-11-03 11:51:33 -0500

    Handling nulls and unsupported types in datatime editor.

--- library/src/main/java/com/northconcepts/templatemaster/form/editor/DateTimeEditor.java
+++ library/src/main/java/com/northconcepts/templatemaster/form/editor/DateTimeEditor.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 
+import com.northconcepts.templatemaster.content.TemplateMasterException;
 import com.northconcepts.templatemaster.form.CrudResource;
 import com.northconcepts.templatemaster.form.FieldDef;
 
@@ -21,6 +22,10 @@ public class DateTimeEditor implements FieldValueEditor {
     
     @Override
     public String getEditValue(CrudResource<?, ?> resource, FieldDef fieldDef, Object entity, Object fieldValue) {
+        if (fieldValue == null) {
+            return "";
+        }
+        
         if (fieldValue instanceof TemporalAccessor) {
             TemporalAccessor temporalAccessor = (TemporalAccessor) fieldValue;
             return formatter.format(temporalAccessor);
@@ -28,7 +33,12 @@ public class DateTimeEditor implements FieldValueEditor {
             Date date = (Date) fieldValue;
             return formatter.format(date.toInstant());
         }
-        return FieldValueEditor.NULL.getEditValue(resource, fieldDef, entity, fieldValue);
+        
+        throw new TemplateMasterException("unsupported field value type " + fieldValue.getClass())
+            .set("resource", resource)
+            .set("fieldDef", fieldDef)
+            .set("entity", entity)
+            ;
     }
 
     @Override
