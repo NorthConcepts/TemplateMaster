47c0fdf0bf96f420e74f3fc16454198b12bded9d
Gautam Joshi <gvjoshi25@gmail.com>
2019-11-20 01:55:44 +0530

    Review comments.

--- library/src/main/java/com/northconcepts/templatemaster/form/param/BigDecimalParam.java
+++ library/src/main/java/com/northconcepts/templatemaster/form/param/BigDecimalParam.java
@@ -2,8 +2,6 @@ package com.northconcepts.templatemaster.form.param;
 
 import java.math.BigDecimal;
 
-import com.northconcepts.templatemaster.content.TemplateMasterException;
-
 public class BigDecimalParam extends Param<BigDecimal> {
 
     public BigDecimalParam(String valueAsString) {
@@ -18,17 +16,13 @@ public class BigDecimalParam extends Param<BigDecimal> {
     protected BigDecimal parse(String valueAsString) throws Throwable {
         if (valueAsString == null) {
             return null;
-        }
+        }       
         return new BigDecimal(valueAsString);
     }
 
     @Override
     protected String format(BigDecimal value) throws Throwable {
         return value.toString();
-    }
-
-    public TemplateMasterException addExceptionProperties(TemplateMasterException exception) {
-        return super.addExceptionProperties(exception);
     }
 
 }
